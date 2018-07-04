8ebfb35aa356b074eae6bd75ae4c2d9dba659eb1
Nikolay Lukyanov <lucas4travel@gmail.com>
2018-07-03 23:24:28 -0700

    created class for session operations

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,12 +15,12 @@ def app(request):
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.group_create(Group(group_name="", group_header="", group_footer=""))
-    app.logout()
+    app.session.logout()
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.group_create(Group(group_name="first_group", group_header="first cool logo", group_footer="first group created for python for QA cource"))
-    app.logout()
+    app.session.logout()
 
